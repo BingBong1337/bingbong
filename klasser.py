e81bc17a6f1def90802cb3f447871666ec914cb1
BingBong1337 <89184110+BingBong1337@users.noreply.github.com>
2021-09-13 15:18:41 +0200

    Add files via upload

--- klasser.py
+++ klasser.py
@@ -1,11 +1,14 @@
 class Elev:
-    def __init__(self,namn,ålder):
+    def __init__(self,namn,ålder,godkänd):
         self.namn = namn
         self.ålder = ålder
+        self.godkänd = godkänd
+        if self.godkänd == True:
+            print('Glad')
+Elev1 = Elev('Kalle',80,True)
+  
 
-Elev1 = Elev('Kalle',80)
-
-print(Elev1.namn)
+print(Elev1.namn,Elev1.ålder)
 
 
 
